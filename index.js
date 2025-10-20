8d02c297e1036fb6bf904f3300682945df66f854
Govind kumar <134090540+GovindKumar04@users.noreply.github.com>
2025-10-20 10:19:23 +0530

    route_Added

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import 'dotenv/config';
 
 const app=express();
-
+app.get('/',(req,res)=>{
+    res.send("<h1>This is the deployed page<h1/>")
+})
 const port = process.env.PORT || 5000
 
 app.listen(port,()=>{
