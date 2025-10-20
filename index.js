d4494147efdb16adbb9035bf81ab800165a653de
Govind kumar <134090540+GovindKumar04@users.noreply.github.com>
2025-10-20 10:25:20 +0530

    route_Added

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app=express();
 app.get('/',(req,res)=>{
     res.send("<h1>This is the deployed page<h1/>")
 })
+app.get('/user',(req,res)=>{
+    res.send("My name is Govind Kumar")
+})
 const port = process.env.PORT || 5000
 
 app.listen(port,()=>{
