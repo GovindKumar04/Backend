ae38af43ccd4199197ac6a7f8d19a3ab5d7efe89
Govind kumar <134090540+GovindKumar04@users.noreply.github.com>
2025-10-22 00:49:38 +0530

    utils_completed

---
+++ Backend_project/src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(stutusCode, data, message="Success"){
+        this.statusCode= stutusCode,
+        this.data=data,
+        this.message=message,
+        this.success=success
+    } 
+}
